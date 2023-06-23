99e5680a945e1e4c83aadb194396fa8a19bbb7c4
Dongliang <wdlfellow@hotmail.com>
2023-06-23 20:47:10 +0800

    refactor and add README

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
+use mygrep::Config;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error: {}", err);
+        process::exit(1)
+    });
 
-    let (query, path) = parse_config(&args);
-
-    fs::read_to_string(path).expect("the file does not exist!!!");
+    if let Err(e) = mygrep::run(config){
+        println!("Error: {}", e);
+        process::exit(1);
+    }
 
 }
 
-fn parse_config(args: &[String]) -> (&str, &str){
-    let query = &args[1];
-    let path = &args[2];
-
-    (query, path)
-}
